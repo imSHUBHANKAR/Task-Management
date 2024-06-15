8fdc13f55ba9252cd65751d7a07543a00326fc5a
SHUBHANKAR KUMAR <shubhankar.kr24@gmail.com>
2024-06-15 13:31:20 +0530

    fix auth1 in nav bar commit

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
-
+import { ClerkProvider } from '@clerk/nextjs';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,10 +11,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
